02b1667da45bf83341977e86d239051a46161a4a
HeeHeeHee <praaa0220@gmail.com>
2023-03-03 16:26:48 +0900

    차트 페이지 1차 완료

--- music-chart/js/chart.js
+++ music-chart/js/chart.js
@@ -1,77 +1,103 @@
 // MAIN 장르별 TOP 5
 const rankDivEl = document.querySelectorAll(".text_div");
+
 for (let i = 0; i < 5; i++) {
   const title_h2_i = document.createElement("h2");
   const artist_h3_i = document.createElement("h3");
+  const img_div_i = document.createElement("div");
   const album_img_i = document.createElement("img");
+  const play_img_i = document.createElement("i");
   rankDivEl[i].appendChild(title_h2_i);
   rankDivEl[i].appendChild(artist_h3_i);
-  rankDivEl[i].appendChild(album_img_i);
+  rankDivEl[i].appendChild(img_div_i);
+  img_div_i.appendChild(album_img_i);
+  img_div_i.appendChild(play_img_i);
   title_h2_i.innerHTML = JSON.parse(JSON.stringify(song_data[i].song));
   artist_h3_i.innerHTML = JSON.parse(JSON.stringify(song_data[i].singer));
   album_img_i.setAttribute(
     "src",
     JSON.parse(JSON.stringify(song_data[i].albumCover))
   );
+  play_img_i.setAttribute("class", "fa-brands fa-youtube");
 }
 for (let i = 5; i < 10; i++) {
   const title_h2_i = document.createElement("h2");
   const artist_h3_i = document.createElement("h3");
+  const img_div_i = document.createElement("div");
   const album_img_i = document.createElement("img");
+  const play_img_i = document.createElement("i");
   rankDivEl[i].appendChild(title_h2_i);
   rankDivEl[i].appendChild(artist_h3_i);
-  rankDivEl[i].appendChild(album_img_i);
-  title_h2_i.innerHTML = JSON.parse(JSON.stringify(song_data[i].song));
-  artist_h3_i.innerHTML = JSON.parse(JSON.stringify(song_data[i].singer));
+  rankDivEl[i].appendChild(img_div_i);
+  img_div_i.appendChild(album_img_i);
+  img_div_i.appendChild(play_img_i);
+  title_h2_i.innerHTML = JSON.parse(JSON.stringify(ballad_data[i - 5].song));
+  artist_h3_i.innerHTML = JSON.parse(JSON.stringify(ballad_data[i - 5].singer));
   album_img_i.setAttribute(
     "src",
-    JSON.parse(JSON.stringify(song_data[i].albumCover))
+    JSON.parse(JSON.stringify(ballad_data[i - 5].albumCover))
   );
+  play_img_i.setAttribute("class", "fa-brands fa-youtube");
 }
 
 for (let i = 10; i < 15; i++) {
   const title_h2_i = document.createElement("h2");
   const artist_h3_i = document.createElement("h3");
+  const img_div_i = document.createElement("div");
   const album_img_i = document.createElement("img");
+  const play_img_i = document.createElement("i");
   rankDivEl[i].appendChild(title_h2_i);
   rankDivEl[i].appendChild(artist_h3_i);
-  rankDivEl[i].appendChild(album_img_i);
-  title_h2_i.innerHTML = JSON.parse(JSON.stringify(song_data[i].song));
-  artist_h3_i.innerHTML = JSON.parse(JSON.stringify(song_data[i].singer));
+  rankDivEl[i].appendChild(img_div_i);
+  img_div_i.appendChild(album_img_i);
+  img_div_i.appendChild(play_img_i);
+  title_h2_i.innerHTML = JSON.parse(JSON.stringify(dance_data[i - 10].song));
+  artist_h3_i.innerHTML = JSON.parse(JSON.stringify(dance_data[i - 10].singer));
   album_img_i.setAttribute(
     "src",
-    JSON.parse(JSON.stringify(song_data[i].albumCover))
+    JSON.parse(JSON.stringify(dance_data[i - 10].albumCover))
   );
+  play_img_i.setAttribute("class", "fa-brands fa-youtube");
 }
 
 for (let i = 15; i < 20; i++) {
   const title_h2_i = document.createElement("h2");
   const artist_h3_i = document.createElement("h3");
+  const img_div_i = document.createElement("div");
   const album_img_i = document.createElement("img");
+  const play_img_i = document.createElement("i");
   rankDivEl[i].appendChild(title_h2_i);
   rankDivEl[i].appendChild(artist_h3_i);
-  rankDivEl[i].appendChild(album_img_i);
-  title_h2_i.innerHTML = JSON.parse(JSON.stringify(song_data[i].song));
-  artist_h3_i.innerHTML = JSON.parse(JSON.stringify(song_data[i].singer));
+  rankDivEl[i].appendChild(img_div_i);
+  img_div_i.appendChild(album_img_i);
+  img_div_i.appendChild(play_img_i);
+  title_h2_i.innerHTML = JSON.parse(JSON.stringify(rap_data[i - 15].song));
+  artist_h3_i.innerHTML = JSON.parse(JSON.stringify(rap_data[i - 15].singer));
   album_img_i.setAttribute(
     "src",
-    JSON.parse(JSON.stringify(song_data[i].albumCover))
+    JSON.parse(JSON.stringify(rap_data[i - 15].albumCover))
   );
+  play_img_i.setAttribute("class", "fa-brands fa-youtube");
 }
 
 for (let i = 20; i < 25; i++) {
   const title_h2_i = document.createElement("h2");
   const artist_h3_i = document.createElement("h3");
+  const img_div_i = document.createElement("div");
   const album_img_i = document.createElement("img");
+  const play_img_i = document.createElement("i");
   rankDivEl[i].appendChild(title_h2_i);
   rankDivEl[i].appendChild(artist_h3_i);
-  rankDivEl[i].appendChild(album_img_i);
-  title_h2_i.innerHTML = JSON.parse(JSON.stringify(song_data[i].song));
-  artist_h3_i.innerHTML = JSON.parse(JSON.stringify(song_data[i].singer));
+  rankDivEl[i].appendChild(img_div_i);
+  img_div_i.appendChild(album_img_i);
+  img_div_i.appendChild(play_img_i);
+  title_h2_i.innerHTML = JSON.parse(JSON.stringify(rb_data[i - 20].song));
+  artist_h3_i.innerHTML = JSON.parse(JSON.stringify(rb_data[i - 20].singer));
   album_img_i.setAttribute(
     "src",
-    JSON.parse(JSON.stringify(song_data[i].albumCover))
+    JSON.parse(JSON.stringify(rb_data[i - 20].albumCover))
   );
+  play_img_i.setAttribute("class", "fa-brands fa-youtube");
 }
 
 // TOP 50
@@ -533,7 +559,7 @@ const swiperNotice = new Swiper(".swiper", {
   autoplay: true,
 });
 
-// 버튼 색 유지
+// 버튼색 유지
 const buttonEl = document.querySelectorAll("button");
 
 function handleClick(event) {
@@ -554,9 +580,10 @@ function init() {
 init();
 
 // 날짜
-const now = new Date(); /*현재 Date 객체 생성*/
+const now = new Date(); // 현재 날짜 및 시간
 const today = {
-  year: now.getFullYear() /*년,일, 월,요일 가져오기*/,
+  /*년,일, 월,요일 가져오기*/
+  year: now.getFullYear(),
   date: now.getDate(),
   month: now.toLocaleDateString("en-US", {
     month: "short",
@@ -566,7 +593,221 @@ const today = {
   }) /*요일 문자열로 가져오기*/,
 };
 
-/*현재에 해당하는 년,월,일,요일을 반복출력*/
+// 차트 시간 출력
+const hours = now.getHours(); // 시간
+console.log(`${hours} : 00`);
+const hoursEl = document.querySelector(
+  ".inner_2 > .rank_list > .top50_top > h2"
+);
+console.log(hoursEl);
+hoursEl.textContent = `${hours} : 00`;
+
+// 현재에 해당하는 년,월,일,요일을 반복출력
 for (let key in today) {
   document.getElementById(key).textContent = today[key];
+}
+
+// 유튜브 모달창
+$(".popupVideo .right__card").click(function () {
+  $(".video-popup").addClass("reveal"),
+    $(".video-popup .video-wrapper").remove(),
+    $(".video-popup").append(
+      "<div class='video-wrapper'><iframe width='560' height='315' src='https://youtube.com/embed/" +
+        $(this).data("video") +
+        "?rel=0&playsinline=1&autoplay=1' allow='autoplay; encrypted-media' allowfullscreen></iframe></div>"
+    );
+}),
+  $(".video-popup-closer").click(function () {
+    $(".video-popup .video-wrapper").remove(),
+      $(".video-popup").removeClass("reveal");
+  });
+
+// 배경 랜덤
+const images = [
+  "newjeans_bg2.jpeg",
+  "aespa_bg1.jpeg",
+  "aespa_bg2.jpeg",
+  "redvelvet_bg1.jpeg",
+];
+//random으로 이미지 나타내기
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+// 배경 이미지 넣는 형식 url('img주소')
+const bgImage = `url(./img/${chosenImage})`;
+// main에 추가
+const chartSectionEl = document.querySelector(".main_section");
+chartSectionEl.style.backgroundImage = bgImage;
+
+$("#btn_now").on("click", function () {
+  $("#rank_inner_1").show();
+  $("#rank_inner_2").hide();
+  $("#rank_inner_3").hide();
+  $("#rank_inner_4").hide();
+  $("#rank_inner_5").hide();
+});
+$("#btn_ballad").on("click", function () {
+  $("#rank_inner_2").show();
+  $("#rank_inner_1").hide();
+  $("#rank_inner_5").hide();
+  $("#rank_inner_3").hide();
+  $("#rank_inner_4").hide();
+});
+$("#btn_dance").on("click", function () {
+  $("#rank_inner_3").show();
+  $("#rank_inner_1").hide();
+  $("#rank_inner_2").hide();
+  $("#rank_inner_5").hide();
+  $("#rank_inner_4").hide();
+});
+$("#btn_rap").on("click", function () {
+  $("#rank_inner_4").show();
+  $("#rank_inner_1").hide();
+  $("#rank_inner_2").hide();
+  $("#rank_inner_3").hide();
+  $("#rank_inner_5").hide();
+});
+$("#btn_rb").on("click", function () {
+  $("#rank_inner_5").show();
+  $("#rank_inner_1").hide();
+  $("#rank_inner_2").hide();
+  $("#rank_inner_3").hide();
+  $("#rank_inner_4").hide();
+});
+
+// 버튼 클릭 시 페이지 상단으로 이동
+$("#btn_top").click(function () {
+  $("html, body").animate({ scrollTop: 0 }, { duration: 1100 });
+});
+
+// 버튼 클릭 시 2번째 화면으로 이동
+$("#btn_bottom").click(function () {
+  $("html, body").animate(
+    { scrollTop: $(document).height() / 3 },
+    { duration: 900 }
+  );
+});
+
+// 버튼 클릭 시 페이지 하단으로 이동
+$("#btn_bottom_2").click(function () {
+  $("html, body").animate(
+    { scrollTop: $(document).height() },
+    { duration: 900 }
+  );
+});
+
+const imgEl = document.querySelectorAll(".rank_div > .text_div > div > img");
+const iEl = document.querySelectorAll(".rank_div > .text_div > div > i");
+
+for (let i = 0; i < 5; i++) {
+  imgEl[i].addEventListener("mouseover", function () {
+    iEl[i].style.opacity = 1;
+    imgEl[i].style.opacity = 0.5;
+  });
+  imgEl[i].addEventListener("mouseleave", function () {
+    iEl[i].style.opacity = 0;
+    imgEl[i].style.opacity = 1;
+  });
+  iEl[i].addEventListener("mouseover", function () {
+    iEl[i].style.opacity = 1;
+    imgEl[i].style.opacity = 0.5;
+  });
+  iEl[i].addEventListener("mouseleave", function () {
+    iEl[i].style.opacity = 0;
+  });
+  imgEl[i].addEventListener("click", function () {
+    window.open(
+      `https://www.youtube.com/results?search_query=${song_data[i].song}`
+    );
+  });
+}
+
+for (let i = 5; i < 10; i++) {
+  imgEl[i].addEventListener("mouseover", function () {
+    iEl[i].style.opacity = 1;
+    imgEl[i].style.opacity = 0.5;
+  });
+  imgEl[i].addEventListener("mouseleave", function () {
+    iEl[i].style.opacity = 0;
+    imgEl[i].style.opacity = 1;
+  });
+  iEl[i].addEventListener("mouseover", function () {
+    iEl[i].style.opacity = 1;
+    imgEl[i].style.opacity = 0.5;
+  });
+  iEl[i].addEventListener("mouseleave", function () {
+    iEl[i].style.opacity = 0;
+  });
+  imgEl[i].addEventListener("click", function () {
+    window.open(
+      `https://www.youtube.com/results?search_query=${ballad_data[i - 5].song}`
+    );
+  });
+}
+
+for (let i = 10; i < 15; i++) {
+  imgEl[i].addEventListener("mouseover", function () {
+    iEl[i].style.opacity = 1;
+    imgEl[i].style.opacity = 0.5;
+  });
+  imgEl[i].addEventListener("mouseleave", function () {
+    iEl[i].style.opacity = 0;
+    imgEl[i].style.opacity = 1;
+  });
+  iEl[i].addEventListener("mouseover", function () {
+    iEl[i].style.opacity = 1;
+    imgEl[i].style.opacity = 0.5;
+  });
+  iEl[i].addEventListener("mouseleave", function () {
+    iEl[i].style.opacity = 0;
+  });
+  imgEl[i].addEventListener("click", function () {
+    window.open(
+      `https://www.youtube.com/results?search_query=${dance_data[i - 10].song}`
+    );
+  });
+}
+
+for (let i = 15; i < 20; i++) {
+  imgEl[i].addEventListener("mouseover", function () {
+    iEl[i].style.opacity = 1;
+    imgEl[i].style.opacity = 0.5;
+  });
+  imgEl[i].addEventListener("mouseleave", function () {
+    iEl[i].style.opacity = 0;
+    imgEl[i].style.opacity = 1;
+  });
+  iEl[i].addEventListener("mouseover", function () {
+    iEl[i].style.opacity = 1;
+    imgEl[i].style.opacity = 0.5;
+  });
+  iEl[i].addEventListener("mouseleave", function () {
+    iEl[i].style.opacity = 0;
+  });
+  imgEl[i].addEventListener("click", function () {
+    window.open(
+      `https://www.youtube.com/results?search_query=${rap_data[i - 15].song}`
+    );
+  });
+}
+
+for (let i = 20; i < 25; i++) {
+  imgEl[i].addEventListener("mouseover", function () {
+    iEl[i].style.opacity = 1;
+    imgEl[i].style.opacity = 0.5;
+  });
+  imgEl[i].addEventListener("mouseleave", function () {
+    iEl[i].style.opacity = 0;
+    imgEl[i].style.opacity = 1;
+  });
+  iEl[i].addEventListener("mouseover", function () {
+    iEl[i].style.opacity = 1;
+    imgEl[i].style.opacity = 0.5;
+  });
+  iEl[i].addEventListener("mouseleave", function () {
+    iEl[i].style.opacity = 0;
+  });
+  imgEl[i].addEventListener("click", function () {
+    window.open(
+      `https://www.youtube.com/results?search_query=${rb_data[i - 20].song}`
+    );
+  });
 }
